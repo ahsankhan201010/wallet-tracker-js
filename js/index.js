47394eb7b06bd2fbba5f127be383916fe89d0eba
ahsankhan201010 <ahsankhan201010@gmail.com>
2020-06-05 07:21:37 +0500

    google auth attached and done with redirecting

--- js/index.js
+++ js/index.js
@@ -1,25 +1,56 @@
 var auth = firebase.auth();
 var firestore = firebase.firestore();
-
 var signinForm = document.querySelector(".signinForm");
 var signupForm = document.querySelector(".signupForm");
+var googleBtn = document.querySelector(".googleBtn");
 
-var singinFormSubmission =async (e) => {
+var googleSignin = async () => {
+  try {
+    var googleProvider = new firebase.auth.GoogleAuthProvider();
+    var {
+      additionalUserInfo: { isNewUser },
+      user: { displayName, uid, email },
+    } = await firebase.auth().signInWithPopup(googleProvider);
+    //if new user then store info in firestore
+    if (isNewUser) {
+      //store user data in firestore
+      var userInfo = {
+        fullName: displayName,
+        email,
+        createdAt: new Date(),
+      };
+      await firestore.collection("users").doc(uid).set(userInfo);
+      console.log("done");
+      //redirect
+      location.assign(`./dashboard.html#${uid}`);
+    } else {
+      console.log("welcome");
+      //redirect
+      location.assign(`./dashboard.html#${uid}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+var singinFormSubmission = async (e) => {
   e.preventDefault();
   try {
     var email = document.querySelector(".signinEmail").value;
     var password = document.querySelector(".signinPassword").value;
-    if(email && password) {
-        //login user
-        var {user: {uid}} = await auth.signInWithEmailAndPassword(email,password)
-        //fetch user info
-        var userInfo = await firestore.collection("users").doc(uid).get();
-        console.log(userInfo.data())
-        //redirect
-        // "/dashboard.html#{uid}"
+    if (email && password) {
+      //login user
+      var {
+        user: { uid },
+      } = await auth.signInWithEmailAndPassword(email, password);
+      //fetch user info
+      var userInfo = await firestore.collection("users").doc(uid).get();
+      console.log(userInfo.data());
+      //redirect
+      location.assign(`./dashboard.html#${uid}`);
     }
   } catch (error) {
-      console.log(error)
+    console.log(error);
   }
 };
 
@@ -30,20 +61,22 @@ var singupFormSubmission = async (e) => {
     var email = document.querySelector(".signupEmail").value;
     var password = document.querySelector(".signupPassword").value;
     if (fullName && email && password) {
-        //create user in auth section
-        var {user:{uid}} = await auth.createUserWithEmailAndPassword(email,password)
-        console.log(uid)
-        //store user data in firestore
-        var userInfo = {
-            fullName,
-            email,
-            createdAt: new Date()
-        }
-        console.log(userInfo)
-        await firestore.collection("users").doc(uid).set(userInfo);
-        console.log("done")
-        //redirect to dashboard page
-        // "/dashboard.html#{uid}"
+      //create user in auth section
+      var {
+        user: { uid },
+      } = await auth.createUserWithEmailAndPassword(email, password);
+      console.log(uid);
+      //store user data in firestore
+      var userInfo = {
+        fullName,
+        email,
+        createdAt: new Date(),
+      };
+      console.log(userInfo);
+      await firestore.collection("users").doc(uid).set(userInfo);
+      console.log("done");
+      //redirect
+      location.assign(`./dashboard.html#${uid}`);
     }
   } catch (error) {
     console.log(error);
@@ -52,3 +85,5 @@ var singupFormSubmission = async (e) => {
 
 signinForm.addEventListener("submit", (e) => singinFormSubmission(e));
 signupForm.addEventListener("submit", (e) => singupFormSubmission(e));
+googleBtn.addEventListener("click", googleSignin);
+
